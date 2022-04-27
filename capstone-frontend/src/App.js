f2305f49fd8533b1bd70f150ca20984f363349d3
Shiloh Ballard <shilohballards@gmail.com>
2022-04-27 14:04:21 -0500

    basic routing

--- capstone-frontend/src/App.js
+++ capstone-frontend/src/App.js
@@ -1,9 +1,17 @@
 import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
+import NotFound from './components/utility/NotFound';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+          <Route path='/home' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
