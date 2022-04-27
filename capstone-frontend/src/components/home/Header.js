f0975688864b17bb678c491c8909771df2013150
Shiloh Ballard <shilohballards@gmail.com>
2022-04-27 14:59:57 -0500

    header bar

---
+++ capstone-frontend/src/components/home/Header.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem } from '@mui/material'
+
+const Header = () => {
+
+  const settings = ['Logout'];
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Chat.App
+          </Typography>
+          
+          <div>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+          </div>
+          
+        </Toolbar>
+      </AppBar>
+  )
+}
+
+export default Header
