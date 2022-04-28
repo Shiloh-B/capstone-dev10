f6f43dff702332574eea82b8fd1708e976f7fff0
Shiloh Ballard <shilohballards@gmail.com>
2022-04-28 13:15:20 -0500

    demo

--- capstone-frontend/src/App.js
+++ capstone-frontend/src/App.js
@@ -1,27 +1,38 @@
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { io } from 'socket.io-client';
 import UserContext from './context/UserContext';
+import SocketContext from './context/SocketContext';
 import Home from './components/home/Home';
 import NotFound from './components/utility/NotFound';
 
 function App() {
 
+  const [socket, setSocket] = useState(null);
   const [user, setUser] = useState({
     email: 'shilohballards@gmail.com',
     username: 'Shiloh'
   });
 
+  useEffect(() => {
+    let s = io('https://06f1-2601-447-8101-6a00-d83e-3fba-45eb-2adb.ngrok.io');
+    setSocket(s);
+  }, []);
+
   return (
     <div className="App">
-      <UserContext.Provider value={[user, setUser]}>
-        <Router>
-          <Routes>
-            <Route path="*" element={<NotFound />} />
-            <Route path='/home' element={<Home />} />
-          </Routes>
-        </Router>
-      </UserContext.Provider>
+      <SocketContext.Provider value={[socket, setSocket]}>
+        <UserContext.Provider value={[user, setUser]}>
+          <Router>
+            <Routes>
+              <Route path="*" element={<NotFound />} />
+              <Route path='/home' element={<Home />} />
+            </Routes>
+          </Router>
+        </UserContext.Provider>
+      </SocketContext.Provider>
+      
     </div>
   );
 }
--- capstone-frontend/src/components/home/chat/ChatContainer.js
+++ capstone-frontend/src/components/home/chat/ChatContainer.js
@@ -1,11 +1,13 @@
 import { TextField, Button } from '@mui/material'
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import UserContext from '../../../context/UserContext';
+import SocketContext from '../../../context/SocketContext';
 import MessageContainer from './MessageContainer';
 
 const ChatContainer = () => {
 
   const [user, setUser] = useContext(UserContext);
+  const [socket, setSocket] = useContext(SocketContext);
   const [message, setMessage] = useState('');
 
   // TODO: WILL DELETE ONCE DB IS SET UP
@@ -16,6 +18,18 @@ const ChatContainer = () => {
 
   const [messages, setMessages] = useState(testData);
 
+  useEffect(() => {
+    if(socket == null) return;
+
+    socket.on('chat message', (msg) => {
+      let newMessages = [...messages];
+      newMessages.push(msg);
+      setMessages(newMessages);
+      console.log(msg);
+    });
+
+  }, [socket]);
+
   const handleMessageChange = (e) => {
     setMessage(e.target.value);
   }
@@ -25,13 +39,16 @@ const ChatContainer = () => {
     let newMessages = [...messages];
     newMessages.push({ username: user.username, message: message})
     setMessages(newMessages);
+
+    // emit broadcast
+    socket.emit('chat message', {username: 'Nik', message: message});
   }
 
   return (
     <div className='chat-container'>
       <MessageContainer messages={messages} />
       <form className='message-form' autoComplete='off' onSubmit={submitMessage}>
-        <TextField label='Say Hello!' variant='outlined' className='message-input' name='message' autoComplete='false' onChange={handleMessageChange} />
+        <TextField label='Say Hello!' variant='outlined' className='message-input' name='message' aria-autocomplete='false' onChange={handleMessageChange} />
         <Button variant='contained' className='submit-message-button' type='submit'>Send</Button>
       </form>
     </div>
