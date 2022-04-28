c36822b6123c93174b43bc132429aca719731ac8
njsutliff <njsutliff@gmail.com>
2022-04-28 11:49:09 -0400

    blank test added, working on jdbc repo

---
+++ capstone-java-backend/src/test/data/UserTemplateRepositoryTest.java
@@ -0,0 +1,28 @@
+package data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTemplateRepositoryTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
