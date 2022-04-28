92acf1db32afcd440b70239e201fd7e09acdf336
Shiloh Ballard <shilohballards@gmail.com>
2022-04-28 11:06:15 -0500

    message containers

---
+++ capstone-frontend/src/components/home/chat/Message.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Message = ({ message, username }) => {
+  return (
+    <div className='single-message-container'>
+
+    </div>
+  )
+}
+
+export default Message
--- capstone-frontend/src/components/home/chat/MessageContainer.js
+++ capstone-frontend/src/components/home/chat/MessageContainer.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react'
+import Message from './Message';
 
-const MessageContainer = ({ chatLog }) => {
+const MessageContainer = () => {
 
-  const [messages, setMessages] = useState([]);
+  // TODO: WILL DELETE ONCE DB IS SET UP
+  const testData = [{
+    username: "Shiloh",
+    message: "Hello World!"
+  }];
+
+  const [messages, setMessages] = useState(testData);
+
+  const logs = messages.map((message, key) => {<Message message={message} key={key} />})
   return (
     <div className='message-container'>
-
+      {logs}
     </div>
   )
 }
