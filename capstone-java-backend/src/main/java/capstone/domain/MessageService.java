4dc5e5a2fcc7077a59a28e2fbe6999d1d5acd1ae
Shiloh Ballard <shilohballards@gmail.com>
2022-04-30 23:21:48 -0500

    message service initial commit

--- capstone-java-backend/src/main/java/capstone/domain/MessageService.java
+++ capstone-java-backend/src/main/java/capstone/domain/MessageService.java
@@ -1,4 +1,27 @@
 package capstone.domain;
 
+import capstone.data.MessageRepository;
+import capstone.models.Message;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MessageService {
+
+    private final MessageRepository messageRepository;
+
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public List<Message> findAll() {
+        return messageRepository.findAll();
+    }
+
+    public Message findById(int messageId) {
+        return messageRepository.findById(messageId);
+    }
+
+
 }
