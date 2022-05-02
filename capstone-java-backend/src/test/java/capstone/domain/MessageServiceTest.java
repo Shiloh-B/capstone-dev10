4b99bea4407507319fc6f24d05a1f682f7dcd548
beckywan <beckywang04@gmail.com>
2022-05-02 15:01:12 -0400

    message service testing completed

--- capstone-java-backend/src/test/java/capstone/domain/MessageServiceTest.java
+++ capstone-java-backend/src/test/java/capstone/domain/MessageServiceTest.java
@@ -27,32 +27,70 @@ class MessageServiceTest {
         when(repository.findById(1)).thenReturn(expected);
         Message actual = service.findById(1);
         assertEquals(expected, actual);
+    // not really necessary but eh
+    }
+
+
+    @Test
+    void shouldAdd() {
+        Message message = makeMessage();
+
+        when(repository.add(message)).thenReturn(message);
+
+        Result<Message> actual = service.add(message);
+        assertEquals(ResultType.SUCCESS, actual.getType());
+        assertEquals(message, actual.getPayload());
     }
 
     @Test
     void shouldNotAddWhenInvalid() {
         Message message = makeMessage();
+
+        message.setMessageId(5);
         Result<Message> result = service.add(message);
         assertEquals(ResultType.INVALID, result.getType());
 
-        message.setMessageId(0);
         message.setRoomId(0);
+        result = service.add(message);
+        assertEquals(ResultType.INVALID, result.getType());
+
         message.setUserId(0);
         result = service.add(message);
         assertEquals(ResultType.INVALID, result.getType());
     }
 
     @Test
-    void shouldNotAddWhenValid() {
-        Message expected = makeMessage();
-        Message arg = makeMessage();
-        arg.setMessageId(0);
+    void shouldUpdate() {
+        Message message = makeMessage();
+        message.setMessageId(2);
 
-        when(repository.add(arg)).thenReturn(expected);
-        Result<Message> result = service.add(arg);
-        assertEquals(ResultType.SUCCESS, result.getType());
+        when(repository.update(message)).thenReturn(true);
+        Result<Message> actual = service.update(message);
+        assertEquals(ResultType.SUCCESS, actual.getType());
+    }
 
-        assertEquals(expected, result.getPayload());
+    @Test
+    void shouldNotUpdateMissing() {
+        Message message = makeMessage();
+        message.setMessageId(30);
+
+        when(repository.update(message)).thenReturn(false);
+        Result<Message> actual = service.update(message);
+        assertEquals(ResultType.NOT_FOUND, actual.getType());
+    }
+
+    @Test
+    void shouldNotUpdateWhenInvalid() {
+        Message message = makeMessage();
+        message.setMessageId(2);
+
+        message.setRoomId(0);
+        Result<Message> actual = service.update(message);
+        assertEquals(ResultType.INVALID, actual.getType());
+
+        message.setUserId(0);
+        actual = service.update(message);
+        assertEquals(ResultType.INVALID, actual.getType());
     }
 
     Message makeMessage() {
