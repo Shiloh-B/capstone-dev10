6cb1d2d63cdeaffea0d085672d634e6fd72d2b0b
Shiloh Ballard <shilohballards@gmail.com>
2022-05-03 10:11:15 -0500

    no send password in get request

--- capstone-java-backend/src/main/java/capstone/controllers/UserController.java
+++ capstone-java-backend/src/main/java/capstone/controllers/UserController.java
@@ -4,6 +4,7 @@ import capstone.models.AppUser;
 import capstone.security.AppUserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
