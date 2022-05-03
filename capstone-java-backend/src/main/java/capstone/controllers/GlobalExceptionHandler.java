43c2e537c8a84f6d1b62c3c434577485dc0156aa
beckywan <beckywang04@gmail.com>
2022-05-03 12:47:10 -0400

    added more exception handling

--- capstone-java-backend/src/main/java/capstone/controllers/GlobalExceptionHandler.java
+++ capstone-java-backend/src/main/java/capstone/controllers/GlobalExceptionHandler.java
@@ -1,13 +1,33 @@
 package capstone.controllers;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.validation.ValidationException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResponseEntity<ErrorResponse> handleException(DuplicateKeyException ex) {
+
+        return new ResponseEntity<ErrorResponse>(
+                new ErrorResponse("Object with same Key already exists"),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ErrorResponse> handleException(ValidationException ex) {
+
+        return new ResponseEntity<ErrorResponse>(
+                new ErrorResponse(ex.getMessage()),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 
     @ExceptionHandler(DataAccessException.class)
     public ResponseEntity<ErrorResponse> handleException(DataAccessException ex) {
