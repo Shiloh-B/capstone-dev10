146b320ed32954b9c58a3c9adf7ea1cf5cdeb22c
njsutliff <njsutliff@gmail.com>
2022-05-04 15:59:18 -0400

    testing started for room service

--- capstone-java-backend/src/test/java/capstone/domain/RoomServiceTest.java
+++ capstone-java-backend/src/test/java/capstone/domain/RoomServiceTest.java
@@ -1,17 +1,35 @@
 package capstone.domain;
 
+import capstone.data.RoomRepository;
+import capstone.models.Room;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class RoomServiceTest {
+
+    @Autowired
+    RoomService service;
+
+    @MockBean
+    RoomRepository repository;
 
     @Test
     void findByUserId() {
+        Room expected = makeRoom();
+        repository.findByUserId(1);
     }
+
 
     @Test
     void findByRoomId() {
+        Room expected = makeRoom();
+        Room result = repository.findByRoomId(1);
+        assertEquals(expected, result);
     }
 
     @Test
@@ -24,5 +42,11 @@ class RoomServiceTest {
 
     @Test
     void deleteByRoomId() {
+    }
+
+    private Room makeRoom() {
+        Room room = new Room();
+        room.setRoomName("test");
+        return  room;
     }
 }
