63a60fc51d6ac8907f15b2b51bce1ff9eb8251cd
njsutliff <njsutliff@gmail.com>
2022-05-04 15:18:10 -0400

    RoomService should be done, now to test it

--- capstone-java-backend/src/main/java/capstone/domain/RoomService.java
+++ capstone-java-backend/src/main/java/capstone/domain/RoomService.java
@@ -9,46 +9,60 @@ import java.util.List;
 @Service
 public class RoomService {
 
-    private  final RoomRepository roomRepository;
+    private final RoomRepository roomRepository;
 
     public RoomService(RoomRepository roomRepository) {
         this.roomRepository = roomRepository;
     }
 
-    public List<Room> findByUserId(int AppUserId){
-        return  roomRepository.findByUserId(AppUserId);
+    public List<Room> findByUserId(int AppUserId) {
+        return roomRepository.findByUserId(AppUserId);
     }
-    public Room findByRoomId(int roomId){
-        return  roomRepository.findByRoomId(roomId);
+
+    public Room findByRoomId(int roomId) {
+        return roomRepository.findByRoomId(roomId);
     }
-    public Result<Room> add(Room room){
+
+    public Result<Room> add(Room room) {
         Result<Room> result = validate(room);
-        if(!result.isSuccess()){
-            return  result;
+        if (!result.isSuccess()) {
+            return result;
         }
         room = roomRepository.add(room);
         result.setPayload(room);
-        return  result;
+        return result;
     }
-    public Result<Room> update(Room room){
+
+    public Result<Room> update(Room room) {
         Result<Room> result = validate(room);
-        return  result;
+        if (!result.isSuccess()) {
+            return result;
+        }
+        if (room.getRoomId() < 1) {
+            result.addMessage("Need a room Id for update operation", ResultType.INVALID);
+        }
+        if (!roomRepository.update(room)) {
+            result.addMessage("Room not found", ResultType.NOT_FOUND);
+        }
+        return result;
     }
-    public boolean deleteByRoomId(int roomId){
+
+    public boolean deleteByRoomId(int roomId) {
         return roomRepository.deleteByRoomId(roomId);
     }
-    private Result<Room> validate(Room room){
+
+    private Result<Room> validate(Room room) {
         Result<Room> result = new Result<>();
-        if(room.getRoomName()==(null)){
+        if (room.getRoomName() == (null)) {
             result.addMessage("Room name cannot be null", ResultType.INVALID);
             return result;
         }
-        if(room.getRoomName().equals("")){
+        if (room.getRoomName().equals("")) {
             result.addMessage("Room name cannot be empty", ResultType.INVALID);
         }
-        if(room.getRoomId() < 1){
+        if (room.getRoomId() < 1) {
             result.addMessage("Room id invalid", ResultType.INVALID);
         }
-        return  result;
+        return result;
     }
 }
---
+++ capstone-java-backend/src/test/java/capstone/domain/RoomServiceTest.java
@@ -0,0 +1,28 @@
+package capstone.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomServiceTest {
+
+    @Test
+    void findByUserId() {
+    }
+
+    @Test
+    void findByRoomId() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteByRoomId() {
+    }
+}
