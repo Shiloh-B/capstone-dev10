34fa5a7c739aacf2c3c90f006339ce5644e06e4e
njsutliff <njsutliff@gmail.com>
2022-05-04 16:34:31 -0400

    add test

--- capstone-java-backend/src/test/java/capstone/domain/RoomServiceTest.java
+++ capstone-java-backend/src/test/java/capstone/domain/RoomServiceTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -33,9 +34,22 @@ class RoomServiceTest {
     }
 
     @Test
-    void add() {
+    void shouldAdd() {
+        Room toAdd = makeRoom();
+        when(repository.add(toAdd)).thenReturn(toAdd);
+        Result<Room> result = service.add(toAdd);
+        assertEquals(ResultType.SUCCESS, result.getType());
+        assertEquals(toAdd, result.getPayload());
     }
 
+    @Test
+    void shouldFailToAddInvalid(){
+        Room dontAdd = makeRoom();
+        dontAdd.setRoomId(-2);
+        dontAdd.setRoomName("");
+        Result<Room> result = service.add(dontAdd);
+        assertNotEquals(ResultType.SUCCESS, result.getType());
+    }
     @Test
     void update() {
     }
@@ -46,6 +60,7 @@ class RoomServiceTest {
 
     private Room makeRoom() {
         Room room = new Room();
+        room.setRoomId(1);
         room.setRoomName("test room");
         return  room;
     }
